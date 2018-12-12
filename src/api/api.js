a592c0c3edb31abb55ea5af6f757b8659d2a548e
bethelight <1598204060@qq.com>
2018-12-12 14:42:32 +0800

    finish

--- src/api/api.js
+++ src/api/api.js
@@ -6,8 +6,14 @@ const veriCode = (p) => wxRequest(p, '/code', 'POST')
 // 登录接口
 const login = (p) => wxRequest(p, '/login', 'POST')
 
+// 获取家长所有的孩子信息
+const getChildren = (p) => wxRequest(p, `/parents/${p.id}`, 'GET')
+
+// 获取轮播图
+const getCarousel = (p) => wxRequest(p, '/slideshows', 'GET')
+
 // 获取视频教程接口
-const getVideo = (params) => wxRequest(params, `/courses/${params.courseName}/video`, 'GET')
+const getVideo = (params) => wxRequest(params, `/children/${params.id}/course`, 'GET')
 
 // 签到  疑问？如果连续点击多次的结果
 const signIn = (params) => wxRequest(params, `/children/${params.id}/sign`, 'PUT')
@@ -15,6 +21,8 @@ const signIn = (params) => wxRequest(params, `/children/${params.id}/sign`, 'PUT
 export default {
   veriCode,
   login,
+  getChildren,
+  getCarousel,
   getVideo,
   signIn
 }
